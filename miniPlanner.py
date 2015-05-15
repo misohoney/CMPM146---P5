bf16fcae8db7d1e56eb8ea797a0dea052af62b31
Vi Tu <vttu@ucsc.edu>
2015-05-14 22:53:06 -0700

    fixed some stuffs

--- miniPlanner.py
+++ miniPlanner.py
@@ -1,7 +1,7 @@
 import json
 from heapq import heappush, heappop
 from collections import namedtuple
-with open('crafting.json') as f:
+with open('Crafting.json') as f:
    Crafting = json.load(f)
 
 # List of items that can be in your inventory:
@@ -17,7 +17,7 @@ print "Inventory:", Crafting['Initial']
 print "Goal:", Crafting['Goal']
 # {'stone_pickaxe': 2}
 
-# Dict of crafting recipes (each is a dict):
+# Dict of crafting recipeDict (each is a dict):
 #print Crafting['Recipes']['craft stone_pickaxe at bench']
 # example:
 # { 'Produces': {'stone_pickaxe': 1},
@@ -35,23 +35,22 @@ def make_checker(rule):
       requires = [item for item in rule['Requires'].keys()]
    produce = [(item,rule['Produces'][item]) for item in rule['Produces']]
    def check(state):
-      checkflag = True
       for i,a in produce:
-         if maxInv[i] == 0:
-             checkflag = False
+         if recipeDict[i] == 0:
+             return False
          if i in state:
-            if state[i] >= maxInv[i]:
-                checkflag = False
+            if state[i] >= recipeDict[i]:
+                return False
       for each in requires:
          if each not in state:
-            checkflag = False
+            return False
       for each,amount in consumes:
          if each not in state:
-            checkflag = False
+            return False
          else:
             if (state[each] < amount):
-               checkflag = False
-      return checkflag
+               return False
+      return True
    return check
 
 def make_effector(rule):
@@ -60,7 +59,7 @@ def make_effector(rule):
    if 'Consumes' in rule:
       consumes = [(item,rule['Consumes'][item]) for item in rule['Consumes']]
    if 'Produces' in rule:
-   	  produce = [(item,rule['Produces'][item]) for item in rule['Produces']]
+        produce = [(item,rule['Produces'][item]) for item in rule['Produces']]
    def effect(state):
       returnstate = state.copy()
       for i,a in consumes:
@@ -73,47 +72,53 @@ def make_effector(rule):
    return effect
 
 def search(graph,initial,is_goal):
+   #the amount of goal object
+   for each, amount in Crafting['Goal'].items():
+       recipeDict[each] = amount
+       #print "amount", amount, each, "recipe", recipeDict[each]
    num = 1
-	# print out how many of each item you can have
-   mining = []
+   craftPath = []
    if is_goal(initial):
       return
    else:
       inventory = initial
       adjlist = graph(inventory)
       lookat = []
-
       for steps in adjlist:
-        heappush(lookat, (steps[2], steps, mining))
-
+        heappush(lookat, (steps[2], steps, craftPath))
+        #print "lookat:", lookat, "steps[2]:", steps[2], "steps:", steps, "craftPath", craftPath
       while len(lookat) > 0:
          curr = lookat.pop(0)
-         mining1Copy = []
+         #print "curr:", curr
+         copyPath = []
          for items in curr[2]:
-            mining1Copy.append(items)
+            copyPath.append(items)
          if is_goal(curr[1][1]):
             curr[2].append((curr[1][0], curr[1][1]))
             print ""
-            print "BEGIN MINING AND CRAFTING", Crafting['Goal'] 
+            print "BEGIN PROCESS", Crafting['Goal'] 
+            print ""
             for items in curr[2]:
                 print  "Step", num, ":", items
                 num = num + 1
-            print "FINISHED CRAFTING", Crafting['Goal']
+            print ""
+            print "FINISHED", Crafting['Goal']
             print ""
             return
          num = 1
          adjlist = graph(curr[1][1])
-         mining1Copy.append((curr[1][0], curr[1][1]))
+         copyPath.append((curr[1][0], curr[1][1]))
          heu = steps[2] + curr[0] - 4
          for steps in adjlist:
-         	heappush(lookat, (heu, steps, mining1Copy))
+            heappush(lookat, (heu, steps, copyPath))
       return
    
-def make_graph(state):
-	for r in all_recipes:
-		if r.check(state):
-			yield (r.name, r.effect(state), r.cost, state.copy())
+def graph(state):
+   for r in all_recipes:
+      if r.check(state):
+         yield (r.name, r.effect(state), r.cost, state.copy())
 
+#function to identify if items is already there/made
 def is_goal(state):
    for item, amount in Crafting['Goal'].items():
       if item in state:
@@ -123,30 +128,53 @@ def is_goal(state):
          return False
    return True
 
-def buildInventory():
-	for items in Crafting['Items']:
-	    maxInv[items] = 0
-	for action,rule in Crafting['Recipes'].items():
-	    if 'Requires' in rule:
-	        for each in rule["Requires"].keys():
-	            maxInv[each] = 1
-	    if 'Consumes' in rule:
-	        for each,amount in rule['Consumes'].items():
-	            if maxInv[each] < amount:
-	                maxInv[each] = amount
-	maxInv[u'wooden_axe'] = 0
-	maxInv[u'stone_axe'] = 0
-	maxInv[u'iron_axe'] = 0
-	maxInv[u'iron_pickaxe'] = 0
+#function to add all info to recipeDict 
+def buildDict():
+   #set all value of crafting items to 0
+   print ""
+   print "RECIPES:"
+   print ""
+   for items in Crafting['Items']:
+       recipeDict[items] = 0
+       #print items
+   #check all the rules in recipeDict
+   for action,rule in Crafting['Recipes'].items():
+       print "Action:", action
+       print "Rule:", rule
+       #If there is requirements in rule 
+       #set numbers of required item to 1
+       if 'Requires' in rule:
+           for each in rule["Requires"].keys():
+               recipeDict[each] = 1
+               print "Requires", each, recipeDict[each]
+       #If there is Consumes found in rule
+       #set the amount of Consumes items to the largest number found
+       if 'Consumes' in rule:
+           for each,amount in rule['Consumes'].items():
+               if recipeDict[each] < amount:
+                   recipeDict[each] = amount
+                   print "Consumes", each, recipeDict[each]
+       print ""
+
+
+   #items that are not in requires or consumes to craft other objects
+   recipeDict[u'wooden_axe'] = 0
+   recipeDict[u'stone_axe'] = 0
+   recipeDict[u'iron_axe'] = 0
+   recipeDict[u'iron_pickaxe'] = 0
+   print "RECIPES END"
+   print ""
+
 
 Recipe = namedtuple('Recipe',['name','check','effect','cost'])
 all_recipes = []
-maxInv = {}
-buildInventory()
+recipeDict = {} #store all the recipeDict information
+action = {}
+buildDict() #build a dict of crafting recipeDict
 for name, rule in Crafting['Recipes'].items():
    checker = make_checker(rule)
    effector = make_effector(rule)
    recipe = Recipe(name, checker, effector, rule['Time'])
    all_recipes.append(recipe)
 
-search(make_graph,Crafting['Initial'],is_goal)
+search(graph,Crafting['Initial'],is_goal)
